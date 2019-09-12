a7b93c5ad5a960c0e9ab56657d8d4512f2d7c11b
botahamec <botahamec@outlook.com>
2019-09-12 18:45:39 -0400

    Added a test for Webster.get_definition()

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import webster_py
 
-# test Object.__init__
+# test Object.__init__()
 def object_initialization():
 	food = webster_py.Object("Food", {"Name": None,
 								      "Group": None,
@@ -12,7 +12,7 @@ def object_initialization():
 	for key, value in food.properties.items():
 		print("    " + key + ": " + value.__class__.__name__)
 
-# test Webster.__init__
+# test Webster.__init__()
 def webster_initialization():
 	webster = webster_py.Webster()
 	print("Webster Type: " + webster.__class__.__name__)
@@ -21,12 +21,13 @@ def webster_initialization():
 	print("Memory Length: " + str(len(webster.memory)))
 	print("Dictionary Length: " + str(len(webster.memory)))
 
-# test Webster.add_definition
+# test Webster.add_definition()
 def definition_addition():
 	webster = webster_py.Webster()
 	webster.add_definition("Food", {"Name" : None,
 									"Group": None,
 									"Taste": None})
+	
 	print("Dictionary Length: " + str(len(webster.dictionary)))
 	print("Type: " + webster.dictionary[0].__class__.__name__)
 	print("Name: " + webster.dictionary[0].name)
@@ -34,3 +35,23 @@ def definition_addition():
 	print("Properties:")
 	for key, value in webster.dictionary[0].properties.items():
 		print("    " + key + ": " + value.__class__.__name__)
+
+# test Webster.get_definition()
+def definition_fetch():
+	webster = webster_py.Webster()
+	webster.add_definition("Food", {"Name" : None,
+									"Group": None,
+									"Taste": None})
+	
+	food = webster.get_definition("Food")
+	print("Type: " + food.__class__.__name__)
+	print("Name: " + food.name)
+	print("Properties Type: " + food.properties.__class__.__name__)
+	print("Properties:")
+	for key, value in food.properties.items():
+		print("    " + key + ": " + value.__class__.__name__)
+	
+	try: error = webster.get_definition("Money")
+	except: print("An error returned, as expected")
+
+	print("Dictionary Length: " + str(len(webster.dictionary)))
